ad89c0449a1a6a7252e7bbd9b8787d00ae8c2d6d
mdashikalikhan <khan.ashik@gmail.com>
2026-03-01 15:10:56 +0600

    ts

--- module/src/app.ts
+++ module/src/app.ts
@@ -11,3 +11,25 @@ function add(a:any,b:any){
 let x: number = 10;
 let y: string = "20";
 console.log(x + y);
+
+type User ={
+    name: string;
+    age: number;
+    role?:'admin'| 'guest';
+}
+
+let input = null;
+
+const didProvideInput1 = input || false;
+
+console.log(didProvideInput1);
+
+input = '';
+
+const didProvideInput2 = input || false;
+
+console.log(didProvideInput2);
+
+const didProvideInput3 = input ?? false;
+
+console.log(didProvideInput3);
